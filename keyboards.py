cffb63d990ed4ca503aa4f740f5fa5175887fb03
Lek <ligioner29@mail.ru>
2025-01-23 11:24:53 +0800

    create first_buttom

--- keyboards.py
+++ keyboards.py
@@ -17,3 +17,11 @@ def create_consent_keyboard():
     keyboard.add(accept_buttom, reject_buttom)
 
     return keyboard
+
+
+def create_first_keybords():
+    keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button_1 = telebot.types.KeyboardButton("Я хочу заказать у вас бокс для вещей.")
+    button_2 = telebot.types.KeyboardButton("Я владелец.")
+
+    keyboard.row(button_1, button_2)
