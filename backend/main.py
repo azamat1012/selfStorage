56306fcfa315e941ee3faf52d054fefac7aa7de7
azamat1012 <148556590+azamat1012@users.noreply.github.com>
2025-01-26 14:14:15 +0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -5,8 +5,7 @@ from dotenv import load_dotenv
 import django
 import telebot
 
-
-# Setup Django settings
+#Здесь настройка джанго: у меня почему-то только так работает
 BASE_DIR = Path(__file__).resolve().parent.parent
 sys.path.append(str(BASE_DIR))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "frontend.settings")
@@ -17,22 +16,17 @@ try:
 except ImportError as e:
     print(e)
 
-
 def main():
     load_dotenv()
     token_tg = os.getenv('TG_BOT_TOKEN')
-
     bot = telebot.TeleBot(token_tg)
-
     handle_start(bot)
     handle_messages(bot)
     handle_callbacks(bot)
-
     try:
         bot.polling()
     except Exception as e:
         print(e)
-
 
 if __name__ == "__main__":
     main()
